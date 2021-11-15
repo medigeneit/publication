f2e86680967019cd12cc7b697c9f18b3b946ccb9
Mamba-dot <shahnewaz886@gmail.com>
2021-11-15 19:06:41 +0600

    catrgory module

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => (int) $this->id,
+            'name'          => (string) ($this->name ?? ''),
+            'isMainParent'  => (boolean) (! $this->parent_id),
+            'parentId'      => (int) ($this->parent_id ?? 0),
+            'active'        => (boolean) ($this->active),
+            'parentName'    => (string) ($this->parent->name ?? ''),
+        ];
+    }
+}
