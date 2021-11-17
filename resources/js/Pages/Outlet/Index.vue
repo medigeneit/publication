339595fe4f01b336a40601886ccec3007f115cbb
MKRashed <mkrashed757@gmail.com>
2021-11-17 16:08:32 +0600

    outlet module setup

---
+++ resources/js/Pages/Outlet/Index.vue
@@ -0,0 +1,60 @@
+<template>
+    <Head title="Outlet" />
+
+    <app-layout>
+        <template #header>
+            Outlet List
+        </template>
+
+        <add-new-button :href="route('outlets.create')" />
+
+        <data-table :collections="outlets" :filters="filters" :dateFilter="true">
+            <template #head>
+                <th class="py-3 px-2 text-left">ID</th>
+                <th class="py-3 px-2 text-left">Name</th>
+                <th class="py-3 px-2 text-center">Addres</th>
+                <th class="py-3 px-2 text-center">Phone</th>
+                <th class="py-3 px-2 text-center">Email</th>
+                <th class="py-3 px-2 text-center">Action</th>
+            </template>
+            <template #default="{ item: outlet }">
+                <td class="py-3 px-2 text-left">{{ outlet.id }}</td>
+                <td class="py-3 px-2 text-left">{{ outlet.name }}</td>
+                <td class="py-3 px-2 text-left">{{ outlet.address }}</td>
+                <td class="py-3 px-2 text-left">{{ outlet.phone }}</td>
+                <td class="py-3 px-2 text-left">{{ outlet.email }}</td>
+                <td class="py-2.5 px-2"> 
+                    <div class="flex justify-center items-center gap-1 md:gap-2">
+                        <action-button-show :href="route('outlets.show', outlet.id)" />
+                        <action-button-edit :href="route('outlets.edit', outlet.id)" />
+                    </div>
+                </td>
+            </template>
+        </data-table>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/App.vue";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import DataTable from "@/Components/DataTable.vue";
+import ActionButtonShow from "@/Components/ActionButtonShow.vue";
+import ActionButtonEdit from "@/Components/ActionButtonEdit.vue";
+import AddNewButton from '@/Components/AddNewButton.vue';
+
+export default {
+    components: {
+        AppLayout,
+        DataTable,
+        Head,
+        Link,
+        ActionButtonShow,
+        ActionButtonEdit,
+        AddNewButton,
+    },
+    props: {
+        outlets: { type: Object, default: {} },
+        filters: { type: Object, default: {} },
+    },
+};
+</script>
---
+++ resources/js/Pages/Outlet/Show.vue
@@ -0,0 +1,60 @@
+<template>
+    <Head title="Outlet" />
+
+    <app-layout>
+        <template #header>
+            {{ outlet.name }}
+        </template>
+
+        <add-new-button :href="route('outlets.create')" class="mb-4" />
+        
+        <div class="overflow-auto bg-white border">
+            <table class="table-auto">
+                <show-table-row heading="ID">{{ outlet.id }}</show-table-row>
+
+                <show-table-row heading="Name">{{ outlet.name }}</show-table-row>
+
+                <show-table-row heading="Name">{{ outlet.address }}</show-table-row>
+
+                <show-table-row heading="Name">{{ outlet.phone }}</show-table-row>
+
+                <show-table-row heading="Name">{{ outlet.email }}</show-table-row>
+
+
+                <show-table-row heading="Action">
+                    <div class="flex justify-start items-center gap-1 md:gap-2">
+                        <action-button-edit :href="route('outlets.edit', outlet.id)" />
+                    </div>
+                </show-table-row>
+            </table>
+        </div>
+
+        <div class="w-full mt-4 flex">
+            <go-to-list :href="route('outlets.index')"/>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/App.vue";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import ShowTableRow from "@/Components/ShowTableRow.vue";
+import ActionButtonEdit from "@/Components/ActionButtonEdit.vue";
+import GoToList from '@/Components/GoToList.vue';
+import AddNewButton from '@/Components/AddNewButton.vue';
+
+export default {
+    components: {
+        AppLayout,
+        Head,
+        Link,
+        ShowTableRow,
+        ActionButtonEdit,
+        GoToList,
+        AddNewButton,
+    },
+    props: {
+        outlet: { type: Object, default: {} },
+    },
+};
+</script>
