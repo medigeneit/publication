dcb2cb3972d3990ba09e16879395010108e051e0
msisaif <msi132043@gmail.com>
2021-12-03 19:08:26 +0600

    category multiple

--- resources/js/Pages/Product/Form.vue
+++ resources/js/Pages/Product/Form.vue
@@ -1,138 +1,158 @@
 <template>
-    <div class="w-full max-w-2xl mx-auto p-4 bg-white border shadow rounded">
+    <div class="grid grid-cols-2">
+        <div class="w-full max-w-2xl mx-auto p-4 bg-white border shadow rounded">
 
-        <ValidationErrors class="mb-4" />
+            <ValidationErrors class="mb-4" />
 
-        <form @submit.prevent="submit" class="">
-            <div class="grid grid-cols-2 md:grid-cols-4 gap-x-4">
-                <div class="mb-4 col-span-2">
-                    <Label for="type" value="Type" />
-                    <Select name="type" class="mt-1 block w-full" @change="typeChange(parseInt(form.type))" v-model="form.type" required>
-                        <option value=""> -- Select Type -- </option>
-                        <option :value="type" v-for="(typeName, type) in data.productType" :key="type">{{ typeName }}</option>
-                    </Select>
+            <form @submit.prevent="submit">
+                <div class="grid grid-cols-2 md:grid-cols-4 gap-x-4">
+                    <div class="mb-4 col-span-2">
+                        <Label for="type" value="Type" />
+                        <Select name="type" class="mt-1 block w-full" @change="typeChange(parseInt(form.type))" v-model="form.type" required>
+                            <option value=""> -- Select Type -- </option>
+                            <option :value="type" v-for="(typeName, type) in data.productType" :key="type">{{ typeName }}</option>
+                        </Select>
+                    </div>
+
+                    <div class="mb-4 col-span-2">
+                        <Label for="name" value="Name" />
+                        <Input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus />
+                    </div>
+
+                    <div class="mb-4 col-span-2" id="publisherWrapper">
+                        <Label for="publisher_id" value="Publisher Name" />
+                        <Select id="publisher_id" class="mt-1 block w-full" v-model="form.publisher_id">
+                            <option value=""> -- Select Publisher -- </option>
+                            <option :value="publisherId" v-for="(publisherName, publisherId) in data.publisherList" :key="publisherId">
+                                {{ publisherName }}
+                            </option>
+                        </Select>
+                    </div>
+
+                    <div class="mb-4 hidden col-span-2" id="productWrapper">
+                        <Label for="product_id" value="Product Name" />
+                        <Select id="product_id" class="mt-1 block w-full" v-model="form.product_id">
+                            <option value=""> -- Select Product -- </option>
+                            <option :value="productId" v-for="(productName, productId) in data.productList" :key="productId">
+                                {{ productName }}
+                            </option>
+                        </Select>
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="category_id" value="Category" />
+                        <div class="flex-col">
+                            <Select id="category_id" class="mt-1 w-full" v-model="form.category_id">
+                                <option value=""> -- Select Category -- </option>
+                                <option :value="categoryId" v-for="(categoryName, categoryId) in data.categoryList" :key="categoryId">
+                                    {{ categoryName }}
+                                </option>
+                            </Select>
+                        </div>
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="alert_quantity" value="Add More" />
+                        <button type="button" class="px-2 py-0.5 border rounded bg-gray-600 text-white" @click="multipleCategory">+</button>    
+                    </div>
+                    
+                    <div class="mb-4">
+                        <Label for="alert_quantity" value="Alert Quantity" />
+                        <Input id="alert_quantity" name="alert_quantity" type="number" class="mt-1 block w-full" v-model="form.alert_quantity" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="active" value="Active" />
+                        <Select id="active" name="active" class="mt-1 block w-full" v-model="form.active">
+                            <option value="1">Yes</option>
+                            <option value="0">No</option>
+                        </Select>
+                    </div>
+
+                    <div class="mb-4 col-start-1">
+                        <Label for="edition" value="Edition" />
+                        <Input id="edition" name="edition" type="text" step="0.01" class="mt-1 block w-full" v-model="form.edition" />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="isbn" value="ISBN" />
+                        <Input id="isbn" name="isbn" type="text" step="0.01" class="mt-1 block w-full" v-model="form.isbn" />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="crl" value="Crl" />
+                        <Input id="crl" name="crl" type="text" step="0.01" class="mt-1 block w-full" v-model="form.crl" />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="production_cost" value="Production Cost" />
+                        <Input id="production_cost" name="production_cost" type="number" step="0.01" class="mt-1 block w-full" v-model="form.production_cost" required />
+                    </div>
+
+                    <div class="mb-4 col-start-1">
+                        <Label for="mrp" value="MRP ." />
+                        <Input id="mrp" name="mrp" type="number" step="0.01" class="mt-1 block w-full" v-model="form.mrp" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="distribute_price" value="Distribute Price" />
+                        <Input id="distribute_price" name="distribute_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.distribute_price" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="wholesale" value="Wholesale Price" />
+                        <Input id="wholesale" name="wholesale_price" step="0.01" type="number" class="mt-1 block w-full" v-model="form.wholesale_price" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="retail_price" value="Retail Price" />
+                        <Input id="retail_price" name="retail_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.retail_price" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="special_price" value="Special Price" />
+                        <Input id="special_price" name="special_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.special_price" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="outside_dhaka_price" value="Outside Dhaka Price" />
+                        <Input id="outside_dhaka_price" name="outside_dhaka_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.outside_dhaka_price" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="ecom_distribute_price" value="Ecom. Distribute Price" />
+                        <Input id="ecom_distribute_price" name="ecom_distribute_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.ecom_distribute_price" required />
+                    </div>
+
+                    <div class="mb-4">
+                        <Label for="ecom_wholesale_price" value="Ecom. Wholesale Price" />
+                        <Input id="ecom_wholesale_price" name="ecom_wholesale_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.ecom_wholesale_price" required />
+                    </div>
                 </div>
 
-                <div class="mb-4 col-span-2">
-                    <Label for="name" value="Name" />
-                    <Input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus />
+                <hr class="w-full my-4">
+                
+                <div class="flex items-center justify-between">
+                    <div class="">
+                        <go-to-list :href="route('products.index')"/>
+                    </div>
+                    <Button class="" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        {{ buttonValue }}
+                    </Button>
                 </div>
-
-                <div class="mb-4 col-span-2" id="publisherWrapper">
-                    <Label for="publisher_id" value="Publisher Name" />
-                    <Select id="publisher_id" class="mt-1 block w-full" v-model="form.publisher_id">
-                        <option value=""> -- Select Publisher -- </option>
-                        <option :value="publisherId" v-for="(publisherName, publisherId) in data.publisherList" :key="publisherId">
-                            {{ publisherName }}
-                        </option>
-                    </Select>
-                </div>
-
-                <div class="mb-4 hidden col-span-2" id="productWrapper">
-                    <Label for="product_id" value="Product Name" />
-                    <Select id="product_id" class="mt-1 block w-full" v-model="form.product_id">
-                        <option value=""> -- Select Product -- </option>
-                        <option :value="productId" v-for="(productName, productId) in data.productList" :key="productId">
-                            {{ productName }}
-                        </option>
-                    </Select>
-                </div>
-
-                <div class="mb-4">
-                    <Label for="category_id" value="Category" />
-                    <Select id="category_id" class="mt-1 block w-full" v-model="form.category_id" multiple>
-                        <option value=""> -- Select Category -- </option>
-                        <option :value="categoryId" v-for="(categoryName, categoryId) in data.categoryList" :key="categoryId">
+            </form>
+        </div>
+        <div class="w-full max-w-2xl mx-auto p-4 bg-white border shadow rounded" id="addMoreCategory">
+            <div class="mb-4 w-full">
+                    <Label for="alert_quantity" value="Category List" />
+                    <div class="border-2" :value="categoryId" v-for="(categoryName, categoryId) in data.categoryList" :key="categoryId">
+                        <button class="p-2">
                             {{ categoryName }}
-                        </option>
-                    </Select>
-                </div>
-
-                <div class="mb-4">
-                    <Label for="alert_quantity" value="Alert Quantity" />
-                    <Input id="alert_quantity" name="alert_quantity" type="number" class="mt-1 block w-full" v-model="form.alert_quantity" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="active" value="Active" />
-                    <Select id="active" name="active" class="mt-1 block w-full" v-model="form.active">
-                        <option value="1">Yes</option>
-                        <option value="0">No</option>
-                    </Select>
-                </div>
-
-                <div class="mb-4 col-start-1">
-                    <Label for="edition" value="Edition" />
-                    <Input id="edition" name="edition" type="text" step="0.01" class="mt-1 block w-full" v-model="form.edition" />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="isbn" value="ISBN" />
-                    <Input id="isbn" name="isbn" type="text" step="0.01" class="mt-1 block w-full" v-model="form.isbn" />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="crl" value="Crl" />
-                    <Input id="crl" name="crl" type="text" step="0.01" class="mt-1 block w-full" v-model="form.crl" />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="production_cost" value="Production Cost" />
-                    <Input id="production_cost" name="production_cost" type="number" step="0.01" class="mt-1 block w-full" v-model="form.production_cost" required />
-                </div>
-
-                <div class="mb-4 col-start-1">
-                    <Label for="mrp" value="MRP ." />
-                    <Input id="mrp" name="mrp" type="number" step="0.01" class="mt-1 block w-full" v-model="form.mrp" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="distribute_price" value="Distribute Price" />
-                    <Input id="distribute_price" name="distribute_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.distribute_price" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="wholesale" value="Wholesale Price" />
-                    <Input id="wholesale" name="wholesale_price" step="0.01" type="number" class="mt-1 block w-full" v-model="form.wholesale_price" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="retail_price" value="Retail Price" />
-                    <Input id="retail_price" name="retail_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.retail_price" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="special_price" value="Special Price" />
-                    <Input id="special_price" name="special_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.special_price" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="outside_dhaka_price" value="Outside Dhaka Price" />
-                    <Input id="outside_dhaka_price" name="outside_dhaka_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.outside_dhaka_price" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="ecom_distribute_price" value="Ecom. Distribute Price" />
-                    <Input id="ecom_distribute_price" name="ecom_distribute_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.ecom_distribute_price" required />
-                </div>
-
-                <div class="mb-4">
-                    <Label for="ecom_wholesale_price" value="Ecom. Wholesale Price" />
-                    <Input id="ecom_wholesale_price" name="ecom_wholesale_price" type="number" step="0.01" class="mt-1 block w-full" v-model="form.ecom_wholesale_price" required />
-                </div>
+                        </button>
+                        <span class="float-right pr-3"> &check; </span>
+                    </div>    
             </div>
-
-            <hr class="w-full my-4">
-            
-            <div class="flex items-center justify-between">
-                <div class="">
-                    <go-to-list :href="route('products.index')"/>
-                </div>
-                <Button class="" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    {{ buttonValue }}
-                </Button>
-            </div>
-        </form>
+        </div>
     </div>
 </template>
 
@@ -205,6 +225,10 @@ export default {
                 publisherWrapper.classList.remove('hidden');
                 productWrapper.classList.add('hidden');
             }
+        },
+
+        multipleCategory(event) {
+            document.getElementById('addMoreCategory').classList.remove('hidden')
         },
 
         submit() {
