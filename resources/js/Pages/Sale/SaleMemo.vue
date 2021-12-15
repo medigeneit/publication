0e5324fb36491491738938dfb5d182e53db59be7
msisaif <msi132043@gmail.com>
2021-12-15 19:11:02 +0600

    Sale Memo

--- resources/js/Pages/Sale/SaleMemo.vue
+++ resources/js/Pages/Sale/SaleMemo.vue
@@ -33,7 +33,7 @@
                 </div>
             </div>
 
-            <div class="mb-10">
+            <div class="">
                 <div class="flex gap-2 items-end mb-4">
                     <label for="name">Name</label>
                     <input id="name" type="text" class="border-0 border-dotted border-b-2 border-black focus:ring-0 focus:outline-none w-full" />
@@ -53,45 +53,60 @@
                     </div>
                 </div>
             </div>
-            <table class="w-full border" v-show="priceType">
+
+            <div class="py-4 flex flex-col justify-center items-center gap-1">
+                <span v-for="(message, index) of messages" :key="index" class="text-red-600 text-sm bg-red-200 px-3 py-1 rounded inline-flex gap-2 items-center">
+                    <span>{{ message }}</span>
+                    <span class="text-base cursor-pointer" @click="messages.splice(index, 1)">&times;</span>
+                </span>
+            </div>
+
+            <table class="w-full" v-show="priceType">
                 <thead>
-                    <tr>
-                        <th class="text-center px-2 py-1 w-8">SL</th>
-                        <th class="text-left px-2 py-1">Description</th>
-                        <th class="text-right px-2 py-1 w-20">Quantity</th>
-                        <th class="text-right px-2 py-1 w-24">Unit Price</th>
-                        <th class="text-right px-2 py-1 w-28">Amount</th>
+                    <tr class="border-t">
+                        <th class="text-center px-2 py-2 w-8 border-l border-r">SL</th>
+                        <th class="text-left px-2 py-2 border-r">Description</th>
+                        <th class="text-right px-2 py-2 w-20 border-r">Quantity</th>
+                        <th class="text-right px-2 py-2 w-24 border-r">Unit Price</th>
+                        <th class="text-right px-2 py-2 w-28 border-r">Amount</th>
+                        <th class="text-center px-2 py-2 w-8 border-r"></th>
                     </tr>
                 </thead>
 
                 <tbody>
                     <tr class="border-t" v-for="(saleableProduct, index) in saleableProducts" :key="index">
-                        <td class="text-right px-2 py-1">{{ parseInt(index) + 1 }}</td>
-                        <td>
+                        <td class="text-right px-2 py-1 border-r border-l">
+                            {{ parseInt(index) + 1 }}
+                        </td>
+                        <td class="border-r">
                             <select class="w-full border-0" @change="selectProduct(index)" v-model="saleableProduct.productId">
-                                <!-- <option value="" disabled></option> -->
                                 <option v-for="(product, productId) in products" :key="productId" :disabled="product.disabled" :value="productId">
                                     {{ product.name }}
                                 </option>
                             </select>
                         </td>
-                        <td class="text-right">
+                        <td class="text-right border-r">
                             <input v-show="saleableProduct.productId" type="number" class="w-20 ml-auto text-center border-0" @change="checkQuantity(index)" v-model="saleableProduct.quantity">
                         </td>
-                        <td class="text-right px-2 py-1">
+                        <td class="text-right px-2 py-1 border-r">
                             <span v-show="saleableProduct.productId">
                                 {{ saleableProduct.unitPrice[priceType] }}
                             </span>
                         </td>
-                        <td class="text-right px-2 py-1">
+                        <td class="text-right px-2 py-1 border-r">
                             <span v-show="saleableProduct.productId">
                                 {{ (saleableProduct.quantity * saleableProduct.unitPrice[priceType]) || 0 }}
                             </span>
                         </td>
+                        <td class="border-r">
+                            <div class="flex justify-center items-center py-1">
+                                <button class="text-red-500 text-2xl" @click="removeRow(index)">&times;</button>
+                            </div>
+                        </td>
                     </tr>
                     <tr class="border-t">
-                        <td colspan="2" class="p-2">
-                            <button type="button" @click="addNewRow(Object.keys(saleableProducts).length)" class="border px-2 py-1 rounded">
+                        <td colspan="100" class="py-2 text-right">
+                            <button type="button" @click="addNewRow" class="border px-2 py-1 rounded">
                                + Add Item
                             </button>
                         </td>
@@ -160,19 +175,23 @@ export default {
                     },
                 },
             },
-            saleableProducts: {},
+            saleableProducts: [],
             priceType: 1,
+            messages: [],
         }
     },
     methods: {
-        addNewRow(numberOfIndex) {
-            const newRow = {
+        addNewRow() {
+            this.saleableProducts.push({
                 productId: 0,
-                quantity: 0,
+                quantity: '',
                 unitPrice: {},
-            };
+            });
 
-            this.saleableProducts[numberOfIndex] = newRow;
+            this.removeSelectedProduct();
+        },
+        removeRow(numberOfIndex) {
+            this.saleableProducts.splice(numberOfIndex, 1);
 
             this.removeSelectedProduct();
         },
@@ -205,17 +224,18 @@ export default {
             this.saleableProducts[numberOfIndex].quantity = (product.maxQuantity >= quantity) ? quantity : product.maxQuantity;
 
             if(product.maxQuantity < quantity) {
-                alert(`${product.name} has ${product.maxQuantity} copies in stock.`);
+                this.messages.push(`${product.name} has ${product.maxQuantity} copies in stock.`);
             }
 
         },
         removeSelectedProduct() {
             Object.entries(this.products).forEach((product) => {
                 product[1]['disabled'] = false;
+                product[1]['message'] = '';
             });
 
-            Object.entries(this.saleableProducts).forEach((saleableProduct) => {
-                let selectedProduct = this.products[saleableProduct[1].productId];
+            this.saleableProducts.forEach((saleableProduct) => {
+                let selectedProduct = this.products[saleableProduct.productId];
 
                 selectedProduct ? selectedProduct.disabled = true : '';
             });
