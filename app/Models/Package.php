5b4b1c841ddcce89a363427cc8e33b2d18fc4936
shahnewazIbrahim <shahnewaz886@gmail.com>
2022-05-09 16:29:33 +0600

    testing

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\ScopeDateFilter;
+use App\Traits\ScopeSearch;
+use App\Traits\ScopeSort;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Package extends Model
 {
-    use HasFactory;
+    use HasFactory, ScopeSort, ScopeSearch, ScopeDateFilter,SoftDeletes;
+    protected $guarded = [];
+
+    public function package_products() {
+       return $this->hasMany(PackageProduct::class);
+    }
+    
 }
