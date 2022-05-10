606f3821b089c4dc5e7d391099191ba9e6c56662
Abidur-Rahman-CSE <arahman171275@bscse.uiu.ac.bd>
2022-05-10 11:20:47 +0600

    package BE 3

--- database/migrations/2022_04_28_072846_create_product_requests_table.php
+++ database/migrations/2022_04_28_072846_create_product_requests_table.php
@@ -15,16 +15,11 @@ class CreateProductRequestsTable extends Migration
     {
         Schema::create('product_requests', function (Blueprint $table) {
             $table->id();
-            $table->integer('quantity');
-<<<<<<< HEAD:database/migrations/2022_04_28_072846_create_product_requests_table.php
+            $table->integer('request_quantity');
             $table->unsignedBigInteger('outlet_id')->nullable();
             $table->date('expected_date');
             $table->integer('user_id')->nullable();
-=======
-            $table->integer('storage_id');
-            $table->date('expected_date');
-            $table->unsignedBigInteger('user_id');
->>>>>>> 4b2ccc933da6ab88e83396b6e8cfc28b38b0aac5:database/migrations/2022_04_06_063032_create_product_requests_table.php
+
             $table->softDeletes();
         });
     }
