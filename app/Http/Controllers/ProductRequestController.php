b9c4a7015c61a13a208b228f49ce5926786a5ce2
Abidur-Rahman-CSE <arahman171275@bscse.uiu.ac.bd>
2022-06-29 16:37:58 +0600

    request index --edit

--- app/Http/Controllers/ProductRequestController.php
+++ app/Http/Controllers/ProductRequestController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\ProductRequestResource;
 use App\Models\Circulation;
 use App\Models\ProductRequest;
 use App\Models\RequestResponse;
+use App\Models\Storage;
 use App\Traits\DateFilter;
 use App\Traits\WithProductRelations;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -53,9 +54,11 @@ class ProductRequestController extends Controller
             ->orderBy('expected_date', 'desc');
         // ->search()->filter();
 
+        // return Auth::user()->outlets->pluck('name','id');
         // return $productRequests->get();
         return  ProductRequestResource::collection($productRequests->get());
         return Inertia::render('ProductRequest/Index', [
+            'your_outlets' => Auth::user()->outlets->pluck('name','id'),
             'productRequests' => ProductRequestResource::collection($productRequests->paginate(request()->perpage ?? 100)->onEachSide(1)->appends(request()->input())),
             'filters' => $this->getFilterProperty(),
         ]);
@@ -76,16 +79,21 @@ class ProductRequestController extends Controller
             'request_quantity' => $request->request_quantity,
             'expected_date' => $request->expected_date,
             'type' => $request->type ?? 1,
+            'is_closed' => 0,
+            'outlet_id' => $request->outlet_id ?? null,
             // 'user_id'     => Auth::user()->id
         ]);
         // return $productRequest;
 
+        $outlet = Storage::find( $request->storage_id)->outlet;
         if ($productRequest) {
             RequestResponse::create([
                 'product_request_id' => $productRequest->id,
                 'status' => 1,
                 'note' => $request->note ?? null,
+                'quantity' => $request->request_quantity ?? 0,
                 'user_id' => Auth::user()->id,
+                'outlet_id' =>  $outlet ,
             ]);
         }
 
